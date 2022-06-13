589be1fdf984b10a9733dd84edd28a02b990e841
behruz <behruzqilichevv23@gmail.com>
2022-06-13 13:01:49 +0300

    odd4

--- number_odd.py
+++ number_odd.py
@@ -1,5 +1,10 @@
 #A four-digit integer is given. Find the number of odd digits in it.
-
+x=2356
 #Create a variable "var_int" and assign it a four-digit integer value.
+a =x%10
+b =x//10%10
+c =x//100%10
+d =x//1000%10
+print(a%2+b%2+c%2+d%2)
 
 #Print the number of odd digits in the variable "var_int".
